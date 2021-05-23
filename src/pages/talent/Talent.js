7299f738280c9df1411146d457bebc4f94fa5b6d
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-05-23 20:03:38 +0300

    Add search by location

--- src/pages/talent/Talent.js
+++ src/pages/talent/Talent.js
@@ -121,7 +121,7 @@ class Talent extends CoreEngine {
         const { data, skills, searchkey, loading, count } = this.state;
         let filteredData = data;
         if (searchkey.length > 0) {
-            filteredData = data.filter(i => i.name.toLowerCase().includes(searchkey) || i.description.toLowerCase().includes(searchkey))
+            filteredData = data.filter(i => i.name.toLowerCase().includes(searchkey) || i.description.toLowerCase().includes(searchkey) || i.location.toLowerCase().includes(searchkey))
         }
 
         // Logic for displaying page numbers
